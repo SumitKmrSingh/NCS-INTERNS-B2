944ec548bc75509806a359fbb1e0a70d1d7c466c
SumitKmrSingh <sumitkumar203411@gmail.com>
2026-02-27 12:59:18 +0530

    learn read data

--- src/jdbc/ReadData.java
+++ src/jdbc/ReadData.java
@@ -1,10 +1,14 @@
 package jdbc;
 
+import jdbcCURD.Student;
+
 import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadData {
     public static void main(String[] args) throws Exception{
@@ -17,14 +21,29 @@ public class ReadData {
         String password = "root";
         Connection connection = DriverManager.getConnection(url, username, password);
 
+        List<Student> stdList = new ArrayList<>();
+
         String query = "select *from student";
 
         PreparedStatement ps = connection.prepareStatement(query);
 
         ResultSet rs = ps.executeQuery();
+//        while (rs.next()){
+//            System.out.println("Id : "+rs.getInt(1)+", name : "+rs.getString(2)+", Marks : "+rs.getInt(3));
+//            System.out.println("=================================================================================");
+//        }
+
         while (rs.next()){
-            System.out.println("Id : "+rs.getInt(1)+", name : "+rs.getString(2)+", Marks : "+rs.getInt(3));
-            System.out.println("=================================================================================");
+            Student std = new Student();
+            std.setId(rs.getInt(1));
+            std.setName(rs.getString(2));
+            std.setMarks(rs.getInt(3));
+            stdList.add(std);
+        }
+
+        for (Student std : stdList){
+            System.out.println("Id : "+std.getId()+" name : "+std.getName()+ " marks : "+std.getMarks());
+            System.out.println("========================================================================");
         }
     }
 }
